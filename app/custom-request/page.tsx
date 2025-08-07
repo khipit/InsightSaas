59ffb464f002888e7533fcf447f7d62ebb27169f
khipit <team@khip.io>
2025-08-07 20:16:35 +0900

    Update page.tsx

--- app/custom-request/page.tsx
+++ app/custom-request/page.tsx
@@ -1,16 +1,11 @@
 'use client';
-
 import dynamic from 'next/dynamic';
-
 const CustomRequestPage = dynamic(() => import('./CustomRequestPage'), {
     ssr: false,
 });
-
 export default CustomRequestPage;
-import dynamic from 'next/dynamic';
-
-const CustomRequestPage = dynamic(() => import('./CustomRequestPage'), {
+import dynamic from 'next/dynamic';  // ← 중복!
+const CustomRequestPage = dynamic(() => import('./CustomRequestPage'), {  // ← 중복!
   ssr: false,
 });
-
-export default CustomRequestPage;
+export default CustomRequestPage;  // ← 중복!
